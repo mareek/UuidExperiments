c0181fd0c990de45c1c790e4449b9f4279427696
Matthieu Mourisson <mareek@gmail.com>
2025-02-03 08:52:48 +0100

    added test with more realistic table

--- UuidExperiments/Program.cs
+++ UuidExperiments/Program.cs
@@ -2,4 +2,19 @@
 
 const string LocalDbConnectionString = "Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;";
 
-new SqlServerHelper(LocalDbConnectionString).TestSQLServerOnLocalDb(1_000_000);
+bool smallTable;
+if (args.Length == 0 || string.IsNullOrEmpty(args[0]))
+    smallTable = true;
+else if (string.Equals(args[0], "small", StringComparison.OrdinalIgnoreCase))
+    smallTable = true;
+else if (string.Equals(args[0], "big", StringComparison.OrdinalIgnoreCase))
+    smallTable = false;
+else
+{
+    Console.WriteLine($"Unknown argument : {args[0]}");
+    Console.WriteLine("Aborting...");
+    return;
+}
+
+Console.WriteLine($"starting test with {(smallTable ? "small" : "BIG")} table");
+new SqlServerTestRunner(LocalDbConnectionString).LaunchFullRun(1_000_000, smallTable);
--- UuidExperiments/SqlServerTestRunner.cs
+++ UuidExperiments/SqlServerTestRunner.cs
@@ -1,10 +1,11 @@
 ﻿using System.Numerics;
+using System.Diagnostics;
 using Microsoft.Data.SqlClient;
 using UUIDNext;
 
 namespace UuidExperiment;
 
-public class SqlServerHelper(string instanceConnectionString)
+public class SqlServerTestRunner(string instanceConnectionString)
 {
     const string DatabaseName = "uuidExperimentTestDb";
     const string TableName = "uuidTestTable";
@@ -12,34 +13,30 @@ public class SqlServerHelper(string instanceConnectionString)
     string _instanceConnectionString = instanceConnectionString;
     string _dbConnectionString = $"{instanceConnectionString};Database={DatabaseName}";
 
-    public void TestSQLServerOnLocalDb(int insertCount, int runCount = 10)
+    public void LaunchFullRun(int insertCount, bool smallTable, int runCount = 10)
     {
         if (!TryConnectToServer())
             return;
 
         try
         {
-            using (var serverConnection = OpenServerConnection())
-            {
-                Console.WriteLine("Create DB");
-                ExecuteNonQuery(serverConnection, $"CREATE DATABASE {DatabaseName};");
-            }
+            CreateDb();
 
             decimal fragmentation;
             TimeSpan duration;
 
             Console.WriteLine($"Inserting {insertCount:#,##0} lines using Guid.NewGuid {runCount} times");
-            (fragmentation, duration) = LaunchMultirunTest(runCount, insertCount, Guid.NewGuid);
+            (fragmentation, duration) = LaunchMultirunTest(runCount, insertCount, smallTable, Guid.NewGuid);
             Console.WriteLine($"Fragementation is {fragmentation:#0.0} % in {duration.TotalSeconds:#0.0} s.");
             Console.WriteLine();
 
             Console.WriteLine($"Inserting {insertCount:#,##0} lines using UUIDNext {runCount} times");
-            (fragmentation, duration) = LaunchMultirunTest(runCount, insertCount, () => Uuid.NewDatabaseFriendly(Database.SqlServer));
+            (fragmentation, duration) = LaunchMultirunTest(runCount, insertCount, smallTable, () => Uuid.NewDatabaseFriendly(Database.SqlServer));
             Console.WriteLine($"Fragementation is {fragmentation:#0.0} % in {duration.TotalSeconds:#0.0} s.");
             Console.WriteLine();
 
             Console.WriteLine($"Inserting {insertCount:#,##0} lines using Guid.CreateVersion7 {runCount} times");
-            (fragmentation, duration) = LaunchMultirunTest(runCount, insertCount, Guid.CreateVersion7);
+            (fragmentation, duration) = LaunchMultirunTest(runCount, insertCount, smallTable, Guid.CreateVersion7);
             Console.WriteLine($"Fragementation is {fragmentation:#0.0} % in {duration.TotalSeconds:#0.0} s.");
             Console.WriteLine();
         }
@@ -49,11 +46,18 @@ public class SqlServerHelper(string instanceConnectionString)
         }
     }
 
-    public (decimal fragmentation, TimeSpan duration) LaunchMultirunTest(int runCount, int insertCount, Func<Guid> guidFactory)
+    private void CreateDb()
+    {
+        Console.WriteLine("Create DB");
+        using var serverConnection = OpenServerConnection();
+        ExecuteNonQuery(serverConnection, $"CREATE DATABASE {DatabaseName};");
+    }
+
+    public (decimal fragmentation, TimeSpan duration) LaunchMultirunTest(int runCount, int insertCount, bool smallTable, Func<Guid> guidFactory)
     {
         List<(decimal fragmentation, TimeSpan duration)> results = [];
         for (int i = 0; i < runCount; i++)
-            results.Add(RunTest(insertCount, guidFactory));
+            results.Add(RunTest(insertCount, smallTable, guidFactory));
 
         var medianFrag = GetMedian(results.Select(r => r.fragmentation));
         var medianDurationMs = GetMedian(results.Select(r => r.duration.TotalMilliseconds));
@@ -78,21 +82,41 @@ public class SqlServerHelper(string instanceConnectionString)
         return medianOdd;
     }
 
-    public (decimal fragmentation, TimeSpan duration) RunTest(int insertCount, Func<Guid> guidFactory)
+    public (decimal fragmentation, TimeSpan duration) RunTest(int insertCount, bool smallTable, Func<Guid> guidFactory)
     {
         using var dbConnection = OpenDbConnection();
 
-        ExecuteNonQuery(dbConnection, $"CREATE TABLE {TableName} (UUID uniqueIdentifier NOT NULL, value int NOT NULL, PRIMARY KEY (UUID));");
+        CreateTable(dbConnection, smallTable);
 
-        var chrono = System.Diagnostics.Stopwatch.StartNew();
-        InsertData(dbConnection, insertCount, guidFactory);
-        chrono.Stop();
+        try
+        {
+            Stopwatch chrono = new();
+            InsertData(dbConnection, insertCount, guidFactory, smallTable, chrono);
 
-        var fragmentation = ReadFragmentation(dbConnection);
+            var fragmentation = ReadFragmentation(dbConnection);
 
-        ExecuteNonQuery(dbConnection, $"DROP TABLE {TableName};");
+            return (fragmentation, chrono.Elapsed);
+        }
+        finally
+        {
+            ExecuteNonQuery(dbConnection, $"DROP TABLE {TableName};");
+        }
 
-        return (fragmentation, chrono.Elapsed);
+    }
+
+    private void CreateTable(SqlConnection dbConnection, bool smallTable)
+    {
+        const string smallTableQuery = $"CREATE TABLE {TableName} (UUID uniqueIdentifier NOT NULL, value int NOT NULL, PRIMARY KEY (UUID));";
+        const string bigTableQuery = $"""
+            CREATE TABLE {TableName} 
+                (UUID uniqueIdentifier NOT NULL, 
+                 BirthDate date NOT NULL, 
+                 FirstName nvarchar(20),
+                 LastName nvarchar(30),
+                 Message nvarchar(50),
+                 PRIMARY KEY (UUID));
+            """;
+        ExecuteNonQuery(dbConnection, smallTable ? smallTableQuery : bigTableQuery);
     }
 
     private bool TryConnectToServer()
@@ -122,22 +146,44 @@ public class SqlServerHelper(string instanceConnectionString)
         return connection;
     }
 
-    private void InsertData(SqlConnection connection, int insertCount, Func<Guid> guidFactory)
+    private static readonly DateOnly[] DatePool = [new(2000, 1, 1), new(1975, 12, 5), new(2014, 10, 17), new(1944, 6, 6), new(1952, 1, 28), new(1978, 8, 28), new(1991, 7, 12), new(2008, 10, 12),];
+    private static readonly string[] NamePool = ["Katayun", "Bernd", "Swapna", "Trishna", "Keir", "Borislava", "Ricarda", "Sigismondo", "Marianna", "Doroteja", "Bandile", "Gülizar", "Sieuwerd", "Tarek", "Aleksej"];
+    private const string LoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a elit eros. Aenean mauris mi, euismod non lobortis non, varius id mi. Praesent vehicula suscipit ante molestie euismod. Nunc non tellus ut nisl ullamcorper rutrum. Aenean pharetra gravida varius. Nulla eget metus lobortis, euismod odio.";
+    private void InsertData(SqlConnection connection, int insertCount, Func<Guid> guidFactory, bool smallTable, Stopwatch chrono)
     {
-        const int chunkSize = 100;
+        const int chunkSize = 128;
         var chunks = Enumerable.Range(0, insertCount)
                                .Select(i => (value: i, uuid: guidFactory()))
                                .Chunk(chunkSize);
         foreach (var chunk in chunks)
         {
+
+            var columns = smallTable ? "UUID, value" : "UUID, BirthDate, FirstName, LastName, Message";
+            var valuesToInsert = smallTable
+                ? chunk.Select((d, i) => $"( @uuid{i}, {d.value})")
+                : chunk.Select((_, i) => $"(@uuid{i}, @birthDate{i}, @firstName{i}, @lastName{i}, @message{i})");
+
             var insertCommand = connection.CreateCommand();
+            insertCommand.CommandText = $"INSERT INTO {TableName} ({columns}) VALUES {string.Join(",\n", valuesToInsert)};";
 
-            var valuesToInsert = chunk.Select((d, i) => $"( @uuid{i}, {d.value})");
-            insertCommand.CommandText = $"INSERT INTO {TableName} (UUID, value) VALUES {string.Join(",\n", valuesToInsert)};";
             for (int i = 0; i < chunk.Length; i++)
+            {
                 insertCommand.Parameters.AddWithValue($"@uuid{i}", chunk[i].uuid);
+                if (!smallTable)
+                {
+                    int chunkValue = chunk[i].value;
+                    insertCommand.Parameters.AddWithValue($"@birthDate{i}", DatePool[i % DatePool.Length]);
+                    insertCommand.Parameters.AddWithValue($"@firstName{i}", NamePool[i % NamePool.Length]);
+                    insertCommand.Parameters.AddWithValue($"@lastName{i}", NamePool[chunkValue % NamePool.Length]);
+                    var messageStart = chunkValue % (LoremIpsum.Length - 50);
+                    var messageLength = 20 + i % 30;
+                    insertCommand.Parameters.AddWithValue($"@message{i}", LoremIpsum.Substring(messageStart, messageLength));
+                }
+            }
 
+            chrono.Start();
             insertCommand.ExecuteNonQuery();
+            chrono.Stop();
         }
     }
 
